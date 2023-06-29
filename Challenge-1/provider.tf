cdbc13404bf79d42e5c8515f13bcc5f43e81a00f
Akriti-Bedi <75430414+Akriti-Bedi@users.noreply.github.com>
2023-06-29 18:48:35 +0530

    Update provider.tf

--- Challenge-1/provider.tf
+++ Challenge-1/provider.tf
@@ -4,10 +4,5 @@ provider "azurerm" {
 }
 
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tf-state-demo"
-    storage_account_name = "sttfstatedemo"
-    container_name       = "state"
-    key                  = "L8KX7oxSL7lNKOxafeuxcFBfphw+Rf5I5fIKJpzB58ILlDhE65mIyTZXlGB05s/ZngBiEe4c68NB+AStSit7rQ=="
-  }
+  backend "azurerm" {}
 }
